aa1fb85c73aa871f07d5916533d350c25a6527f2
lucanevess87 <lucatets@hotmail.com>
2023-12-21 21:18:55 -0300

    remove scrollbar

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -5,7 +5,7 @@ import { PlantsTypeTabs } from './components/PlantsTypeTabs';
 
 export const Home = async () => {
   return (
-    <section className="flex flex-col w-full gap-6 overflow-y-auto">
+    <section className="flex flex-col w-full gap-6 overflow-y-auto no-scrollbar">
       <Banner />
       <Search />
       <PlantsTypeTabs />
